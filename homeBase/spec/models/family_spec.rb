c8c07c8ea320fe17d985b658d9f47366a9ee8d4c
Siddharth Watal <sid.watal@gmail.com>
2015-05-28 19:45:14 -0400

    remove minitest, add rspec, and add some family model tests

---
+++ homeBase/spec/models/family_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe Family do 
+	context "validations" do
+		subject { FactoryGirl.create(:family) }
+		it { should validate_presence_of :surname }
+		it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    it { should have_secure_password }
+    it do
+      should validate_length_of(:password).
+        is_at_least(6).is_at_most(20)
+    end
+	end
+end
