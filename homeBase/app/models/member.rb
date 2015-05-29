138bfd5bcb085699603bbd1f193a22525a778660
Siddharth Watal <sid.watal@gmail.com>
2015-05-28 21:12:52 -0400

    more tests

--- homeBase/app/models/member.rb
+++ homeBase/app/models/member.rb
@@ -1,12 +1,11 @@
 class Member < ActiveRecord::Base
-	has_many :medals
 	belongs_to :family
 	has_many :events
 	has_many :rewards
 	has_many :assigned_tasks, foreign_key: :assigned_member_id, class_name: "Task"
 	has_many :completed_tasks, foreign_key: :completed_member_id, class_name: "Task"
 	
-	validates :role, :name, :family, :img_url, presence: true
+	validates :role, :name, :family, :img_url, :color, presence: true
 	validates :points, numericality: {only_integer: true}
 
 end
