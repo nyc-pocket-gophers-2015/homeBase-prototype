7953a5dce7513e96ec126d07653e83adb2be49a7
fishermanng <sherman.ng10@gmail.com>
2015-05-29 12:03:28 -0400

    Update models to be dependent on each other during destroy

--- homeBase/app/models/family.rb
+++ homeBase/app/models/family.rb
@@ -1,9 +1,9 @@
 class Family < ActiveRecord::Base
 	has_secure_password
-	has_many :members
-	has_many :events
-	has_many :rewards
-	has_many :tasks
+	has_many :members, dependent: :destroy
+	has_many :events, dependent: :destroy
+	has_many :rewards, dependent: :destroy
+	has_many :tasks, dependent: :destroy
 
 	validates :surname, :email, :password, presence: true
 	validates :email, uniqueness: true
