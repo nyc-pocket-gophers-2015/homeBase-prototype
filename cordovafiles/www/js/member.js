3b1dd84c957769ee96b7f99c1527529388ef6478
fishermanng <sherman.ng10@gmail.com>
2015-05-30 20:24:26 -0400

    Add member model

---
+++ cordovafiles/www/js/member.js
@@ -0,0 +1,16 @@
+function Member(memberParams){
+  this.role = memberParams.role;
+  this.name = memberParams.name;
+  this.points = memberParams.points;
+  this.color = memberParams.color;
+  this.imgUrl = memberParams.imgUrl;
+  this.pendingRewards = memberParams.pendingRewards;
+  this.assignedTasks = memberParams.assignedTasks;
+  this.completedTasks = memberParams.completedTasks;
+  this.events = memberParams.events;
+}
+
+
+
+
+
